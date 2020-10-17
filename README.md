fa1776486f38d2f993008ec45e8eb820aa349a04
Peter F. Tumulty <ptumulty@sh-law.com>
2020-10-16 21:56:57 -0400

    rebuild the website

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
-# Using JavaScripts's Map & forEach Methods
+# Comparing Array Methods in JavaScript
+
+This is a simple demonstration on how to use ```map()``` and ```forEach()``` methods in javascript. and explaination of how they work, and a comparission between these two array methods.
+
+## Motivation
+
+## Build status
+
+## Installation
+
+
+
+
+
+
 
 This code compares how **.map** and **.forEach** handle the same set of data. The intention of comparison is to discover how .map works on a deeper level. I wrote a full article detailing what I discovered on Medium.com. [Click here to read the full article](https://medium.com/@petertumulty/the-power-of-the-map-method-4db6b1a73655) 
--- app.js
+++ app.js
@@ -1,41 +1,15 @@
-$(document).ready(function() {
+const myWebStack = ['JavaScript', 'HTML', 'CSS', 'somtimes PHP', 'sometimes Python', 'sometimes SaSS'];
 
-    function TransformArray(element) {
-        return element.first.toLowerCase();
-        return element.last.toLowerCase();
-    }
+// the DOM element I'm going to set my results too. I want to display the results of the forEach method
+// in HTML.
+const firstMapElm = document.getElementById('first-foreach');
 
-    $.getJSON("https://immense-gorge-15710.herokuapp.com/experts", function(data) {
-
-        var data = data;
-        console.log("So what's the difference between forEach & .map ?");
-        console.log("--------------")
-
-        data.forEach(function(value, index, array) {
-            console.log("forEach: using value: " + value.first);
-            console.log("forEach: using array[index]: " + array[index].first);
-        })
-
-        console.log('------------');
-        console.log('Using forEach, array[index] returns the same out put as value.');
-        console.log('------------');
-
-        data.map(function(value, index, array) {
-            console.log(".map: using value: " + value.first)
-            console.log(".map: array[index]: " + array[index].first);
-        })
-
-        console.log('------------');
-        console.log('Using .map, array[index] returns the same out put as value.');
-        console.log('------------');
-        console.log("Why bother having a .map and a forEach? Why not just have one or the other? ");
-        console.log("As you can see that both the forEach and .map both take in three arguments and do exactly the same thing. So there really isn't a need for both, right? Ah not necessarily...")
-        console.log('------------');
-        console.log(data.forEach(TransformArray));
-        console.log("The undefined you are seeing above occurs because we are trying to run the TransformArray function on the data in the forEach.");
-        console.log("Why doesn't this work? It doesn't work because the forEach method is simple and only spits back what you put in. The callback function inside the forEach method is designed to only spit back the data you put in it.")
-        console.log(data.map(TransformArray));
-        console.log("The data you are seeing is when you run the TransfromArray function inside the .map parameters. It works because the callback function inside the .map parameters is designed for the data being passed in it to be mutuble.");
-
-    });
+// executing the forEach method and setting the results to a variable called executingForEachOverItems
+const t = myWebStack.forEach(function(item, index, array) {
+	return item;
 });
+
+console.log(t);
+
+// adding the results of the map method over my array to HTML
+firstMapElm.innerHTML= JSON.stringify(t)
